72c75e6e5c03f69b5573759f7dc1d2b7a9b0e433
Stian Johansen <stian.a.johansen@gmail.com>
2021-01-16 21:19:25 +0100

    more experimenting

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -2,7 +2,10 @@ defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-   redirect(conn, to: Routes.page_path(conn, :redirect_test))
+    conn
+    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    |> put_flash(:error, "Let's pretend we have an error.")
+    |> redirect(to: Routes.page_path(conn, :redirect_test))
   end
 
   def redirect_test(conn, _params) do
