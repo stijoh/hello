396a885d4a960da52739800a2b92d917570fe98a
Stian Johansen <stian.a.johansen@gmail.com>
2021-01-16 21:13:41 +0100

    testing some redirects

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-    render(conn, :index)
+   redirect(conn, to: Routes.page_path(conn, :redirect_test))
+  end
+
+  def redirect_test(conn, _params) do
+    render(conn, "index.html")
   end
 end
